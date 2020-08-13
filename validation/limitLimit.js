e040522253a83e5bcce6e8305453b88d2cc2cab5
Nabil <nabil.tharwat@outlook.com>
2020-08-13 13:58:16 +0200

    feat: Added validation limits for 'limit' queries

---
+++ validation/limitLimit.js
@@ -0,0 +1,36 @@
+const { RateLimitError } = require('../errors')
+
+const limitLimit = limit => validationContext => {
+	const { definitions } = validationContext.getDocument()
+	let found = {}
+
+	definitions.forEach(def => {
+		if (def.operation !== 'query') return
+
+		const selections = def.selectionSet.selections.filter(
+			selection => selection.arguments
+		)
+
+		const limitSelection = selections.find(selection =>
+			selection.arguments.find(arg => arg.name.value === 'limit')
+		)
+
+		if (!limitSelection) return
+
+		found = limitSelection.arguments.find(
+			arg =>
+				arg.name.value === 'limit' &&
+				(arg.value.value > limit || arg.value.value < 1)
+		)
+	})
+
+	if (found) {
+		validationContext.reportError(
+			new RateLimitError(`[RATE_LIMIT] Cannot query more than ${limit}`)
+		)
+	}
+
+	return validationContext
+}
+
+module.exports = limitLimit
