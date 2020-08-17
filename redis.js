9f9b3ecfb4abdb2a8fa0d9a16ed3cba00ee5cf8d
Nabil <nabil.tharwat@outlook.com>
2020-08-17 08:09:42 +0200

    config(redis): configured redis

---
+++ redis.js
@@ -0,0 +1,18 @@
+const { promisify } = require('util')
+const redis = require('redis')
+
+const client = redis.createClient({ db: 'discriminatory01' })
+
+client.on('error', error => {
+	// eslint-disable-next-line no-console
+	console.error(error)
+})
+
+const get = promisify(client.get).bind(client)
+const set = promisify(client.set).bind(client)
+
+module.exports = {
+	get,
+	set,
+	client
+}
